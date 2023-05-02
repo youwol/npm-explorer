52c4cb6e694f5c7cbffff6b28a7fbe0b2c28e77d
youwol-guillaume <greinisch@youwol.com>
2023-05-02 17:59:53 +0200

    🎉 auto-generated by TS pipeline

---
+++ src/auto-generated.ts
@@ -0,0 +1,116 @@
+
+const runTimeDependencies = {
+    "externals": {
+        "@youwol/cdn-client": "^1.0.2",
+        "@youwol/flux-view": "^1.0.3",
+        "rxjs": "^6.5.5"
+    },
+    "includedInBundle": {}
+}
+const externals = {
+    "@youwol/cdn-client": "window['@youwol/cdn-client_APIv1']",
+    "@youwol/flux-view": "window['@youwol/flux-view_APIv1']",
+    "rxjs": "window['rxjs_APIv6']"
+}
+const exportedSymbols = {
+    "@youwol/cdn-client": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/cdn-client"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    },
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    }
+}
+
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
+    "entryFile": "./main.ts",
+    "loadDependencies": [
+        "@youwol/cdn-client",
+        "@youwol/flux-view",
+        "rxjs"
+    ]
+}
+
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
+const entries = {
+     '@youwol/npm-explorer': './main.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/npm-explorer/${e.name}`]:e.entryFile}), {})
+}
+export const setup = {
+    name:'@youwol/npm-explorer',
+        assetId:'QHlvdXdvbC9ucG0tZXhwbG9yZXI=',
+    version:'0.1.0-wip',
+    shortDescription:"",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/npm-explorer',
+    npmPackage:'https://www.npmjs.com/package/@youwol/npm-explorer',
+    sourceGithub:'https://github.com/youwol/npm-explorer',
+    userGuide:'https://l.youwol.com/doc/@youwol/npm-explorer',
+    apiVersion:'01',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+    secondaryEntries,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/npm-explorer_APIv01`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
+        const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/npm-explorer#0.1.0-wip~dist/@youwol/npm-explorer/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/npm-explorer/${entry.name}_APIv01`]
+        })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+    }
+}
