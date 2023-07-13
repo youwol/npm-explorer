7813e8d4ed119462bb003107678ae89d44298fd2
youwol-guillaume <greinisch@youwol.com>
2023-07-13 16:38:26 +0200

    :alien: sync python configs w/ new `py-youwol` organization

--- template.py
+++ template.py
@@ -1,53 +1,59 @@
 import shutil
 from pathlib import Path
 
-from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
-    RunTimeDeps, generate_template, DevServer, Bundles, MainModule
-from youwol_utils import parse_json
+from youwol.pipelines.pipeline_typescript_weback_npm import (
+    Template,
+    PackageType,
+    Dependencies,
+    RunTimeDeps,
+    generate_template,
+    DevServer,
+    Bundles,
+    MainModule,
+)
+from youwol.utils import parse_json
 
 folder_path = Path(__file__).parent
 
-pkg_json = parse_json(folder_path / 'package.json')
+pkg_json = parse_json(folder_path / "package.json")
 
 externals = {
     "@youwol/cdn-client": "^1.0.2",
     "@youwol/flux-view": "^1.0.3",
     "rxjs": "^6.5.5",
-    "@youwol/os-top-banner": "^0.1.2"
+    "@youwol/os-top-banner": "^0.1.2",
 }
 
 template = Template(
     path=folder_path,
     type=PackageType.Application,
-    name=pkg_json['name'],
-    version=pkg_json['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
-    dependencies=Dependencies(
-        runTime=RunTimeDeps(
-            externals=externals
-        )
-    ),
+    name=pkg_json["name"],
+    version=pkg_json["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
+    dependencies=Dependencies(runTime=RunTimeDeps(externals=externals)),
     bundles=Bundles(
         mainModule=MainModule(
-            entryFile='./main.ts',
-            loadDependencies=list(externals.keys())
+            entryFile="./main.ts", loadDependencies=list(externals.keys())
         )
     ),
     userGuide=True,
-    devServer=DevServer(
-        port=3019
-    )
+    devServer=DevServer(port=3019),
 )
 
 generate_template(template)
 shutil.copyfile(
-    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
-    dst=folder_path / 'src' / 'auto-generated.ts'
+    src=folder_path / ".template" / "src" / "auto-generated.ts",
+    dst=folder_path / "src" / "auto-generated.ts",
 )
-for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts']:
-    shutil.copyfile(
-        src=folder_path / '.template' / file,
-        dst=folder_path / file
-    )
+for file in [
+    "README.md",
+    ".gitignore",
+    ".npmignore",
+    ".prettierignore",
+    "LICENSE",
+    "package.json",
+    "tsconfig.json",
+    "webpack.config.ts",
+]:
+    shutil.copyfile(src=folder_path / ".template" / file, dst=folder_path / file)
